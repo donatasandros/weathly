2dc0fd913d4e2bda9aba89c497d41aca862b0862
donatas <donatas.andros@gmail.com>
2025-02-15 20:46:05 +0200

    add vercel speed insights

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+	injectSpeedInsights();
+
+	let { children } = $props();
+</script>
+
+{@render children()}
